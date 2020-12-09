be0566c6852abe9603830f7bd0101805cc3a949d
Romain <romain.sire@gmx.com>
2020-12-09 17:46:16 +0100

    add and style background component, image can change with prop

---
+++ src/components/ReactiveBackground.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="blury-background" :style="BGstyle"></div>
+    <div class="clear-background" :style="BGstyle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ReactiiveBackground',
+  props: {
+    imageUrl: String
+  },
+  computed: {
+    BGstyle() {
+      return `background-image: url('${this.imageUrl}')`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.blury-background {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: center/cover no-repeat;
+  filter: blur(10px) sepia(60%);
+  -webkit-filter: blur(10px) sepia(60%);
+}
+.clear-background {
+  position: absolute;
+  top: 15%;
+  right: 15%;
+  bottom: 20%;
+  left: 15%;
+  background: center/cover no-repeat;
+  box-shadow: 0 0 20px #555;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ReactiveBackground
+      imageUrl="https://media.routard.com/image/01/4/fb-perpignan.1543014.jpg"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ReactiveBackground from '@/components/ReactiveBackground.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    ReactiveBackground
   }
 }
 </script>

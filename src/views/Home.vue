dde235193f27d7b5cf30c9523629d935291a9fc2
Romain <romain.sire@gmx.com>
2020-12-10 20:29:42 +0100

    use Axios instead of Fetch to call API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,15 @@
       imageUrl="https://media.routard.com/image/01/4/fb-perpignan.1543014.jpg"
     />
     <CityName />
-    <WeatherData />
+    <WeatherData v-bind:weather="weather" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ReactiveBackground from '@/components/ReactiveBackground.vue'
-import CityName from '../components/CityName.vue'
-import WeatherData from '../components/WeatherData'
+import CityName from '@/components/CityName.vue'
+import WeatherData from '@/components/WeatherData'
 
 export default {
   name: 'Home',
@@ -23,22 +23,49 @@ export default {
   },
   data: function() {
     return {
-      location: 'Perpignan'
+      location: 'Perpignan',
+      weather: {}
     }
   },
   methods: {
     getWeatherInfos() {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=perpignan&appid=${process.env.VUE_APP_OPEN_WEATHER_KEY}&units=metric&lang=fr`
-      )
-        .then(response => response.json())
-        .then(data => {
-          console.log(data)
-        })
+      const axios = require('axios').default
+      axios
+        .all([
+          axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?q=perpignan&appid=${process.env.VUE_APP_OPEN_WEATHER_KEY}&units=metric&lang=fr`
+          ),
+          axios.get(
+            `https://api.openweathermap.org/data/2.5/forecast?q=perpignan&appid=${process.env.VUE_APP_OPEN_WEATHER_KEY}&units=metric&lang=fr`
+          )
+        ])
+        .then(
+          axios.spread((currentData, forecastData) => {
+            this.weather.currentTemp = currentData.data.main.temp
+            this.weather.icon = currentData.data.weather[0].icon
+            this.weather.wind = currentData.data.wind.speed
+            this.weather.pressure = currentData.data.main.pressure
+            this.weather.humidity = currentData.data.main.humidity
+            this.weather.threeHours = {
+              time: forecastData.data.list[1].dt_txt,
+              icon: forecastData.data.list[1].weather[0].icon,
+              temp: forecastData.data.list[1].main.temp
+            }
+            this.weather.sixHours = {
+              time: forecastData.data.list[2].dt_txt,
+              icon: forecastData.data.list[2].weather[0].icon,
+              temp: forecastData.data.list[2].main.temp
+            }
+            this.weather.twentyfourHours = {
+              time: forecastData.data.list[8].dt_txt,
+              icon: forecastData.data.list[8].weather[0].icon,
+              temp: forecastData.data.list[8].main.temp
+            }
+          })
+        )
     }
   },
-  created: function() {
-    // `this` est une référence à l'instance de vm
+  mounted: function() {
     this.getWeatherInfos()
   }
 }

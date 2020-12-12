51b85c89f89e8aceea6a2aa8c95cd49321f74c66
Romain <romain.sire@gmx.com>
2020-12-12 19:24:56 +0100

    router management, and redirections

--- src/router/index.js
+++ src/router/index.js
@@ -5,23 +5,17 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: '/Montpellier' },
   {
-    path: '/',
+    path: '/:location',
     name: 'Home',
     component: Home
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '*', redirect: '/Montpellier' }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 })
 

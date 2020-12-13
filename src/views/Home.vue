535f398ee343c2c88b9ae29e8f6de1bb7d7388d6
Romain <romain.sire@gmx.com>
2020-12-13 14:49:48 +0100

    bugfix: when the location changes, recharges the weather data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,13 @@
       imageUrl="https://media.routard.com/image/01/4/fb-perpignan.1543014.jpg"
     />
     <CityName />
-    <WeatherData />
+    <WeatherData :key="location" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { mapActions } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import ReactiveBackground from '@/components/ReactiveBackground.vue'
 import CityName from '@/components/CityName.vue'
 import WeatherData from '@/components/WeatherData'
@@ -21,6 +21,9 @@ export default {
     ReactiveBackground,
     CityName,
     WeatherData
+  },
+  computed: {
+    ...mapState(['location'])
   },
   methods: {
     ...mapActions(['updateLocation'])
